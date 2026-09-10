a4e287955440e86463473137050b3c1885058ba3
abuyaser20061995-sudo <abuyaser20061995@gmail.com>
2026-09-10 17:05:30 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import { SettingsProvider } from './context/SettingsContext.jsx'
+import './styles/theme.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SettingsProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </SettingsProvider>
+  </React.StrictMode>
+)
